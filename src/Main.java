94ef6e4041a13c52ac25a79934db3ef54911b75c
JeremiasArian <159722626+JeremiasArian@users.noreply.github.com>
2024-09-11 17:54:41 -0300

    Create products table

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,24 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        try (var connection =  DB.connect()){
+        try (var connection = DB.connect()) {
             System.out.println("Connected to the PostgresSQL database.");
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
+
+        var sql = "CREATE TABLE products (" +
+                "    id SERIAL PRIMARY KEY," +
+                "    name VARCHAR(255) NOT NULL," +
+                "    price DECIMAL(10, 2) NOT NULL" +
+                ");";
+
+        try (var connection = DB.connect();
+             var statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("Created products table");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
-    }
+}
